bc5699bd84296acf38362941f84abb2bae389228
Mwesigai <mwesigwai433@gmail.com>
2025-01-10 14:29:15 +0300

    spaci

--- app/components/homeNavItems.tsx
+++ app/components/homeNavItems.tsx
@@ -8,7 +8,7 @@ type HomeNavigationProps = {
   
 export default function HomeNavigationItems({items}: HomeNavigationProps) {
   return (
-    <div className="sm:flex items-center space-x-4 shrink-0">
+    <div className="sm:flex items-center space-x-3 shrink-0">
         {
             items.map((item, index) => (
                 <Link key={index} href={item.targetRef} className="text-sm border-slate-300 border px-5 py-2 dark:border-slate-400 rounded-3xl text-slate-700 dark:text-slate-300">{item.itemName}</Link>
